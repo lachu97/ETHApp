7f5f485153d9fc395033161af8aeeac1d19274a5
Lakshu Narasu <ced15i004@iiitdm.ac.in>
2024-01-23 15:01:20 +0530

    initial setup

---
+++ AppModules/DevConfig/apiKeys.js
@@ -0,0 +1,4 @@
+export const API_KEY="anPZsLlwd-NdUtbSE0fBbJ7PEXjV5OX9"
+export const URL = "https://eth-goerli.g.alchemy.com/v2/anPZsLlwd-NdUtbSE0fBbJ7PEXjV5OX9"
+
+export const WEB_SOCKET = "wss://eth-goerli.g.alchemy.com/v2/anPZsLlwd-NdUtbSE0fBbJ7PEXjV5OX9"
